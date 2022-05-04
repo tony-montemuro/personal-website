4f71dc2ae986cf5a386e1bf833349387b67e9487
Tony Montemuro <tonyamontemuro@gmail.com>
2022-05-04 10:35:38 -0500

    finished achievemnts page

--- src/components/achievements/Achievements.jsx
+++ src/components/achievements/Achievements.jsx
@@ -26,7 +26,7 @@ const Achievements = () => {
             </div>
             <div className="v-desc">
                 <h2 className="v-desc-title">
-                    number 4: E40 - 5000 (tied wr)
+                    number 4: E40 - 5000 (Tied WR)
                 </h2>
                 <p className="v-desc-desc">
                     for any monkey ball player who really gets into ils, this is one of those ties that everyone
@@ -49,11 +49,34 @@ const Achievements = () => {
             </div>
             <div className="v-desc">
                 <h2 className="v-desc-title">
-                    number 3: overwatch - 3602 sr on support (masters rank)
+                    number 3: Overwatch - 3602 SR Support (Masters Rank)
                 </h2>
+                <p className="v-desc-desc">
+                    of all the achievements i have listed on this page, this one is definitely the odd one out. if you just happened
+                    to stumble on this page, or you do not know me all that well, you may just think i am a speedrunner. however, i
+                    actually really enjoy fps games; in particular, i really like team-based shooters. games similar to overwatch that
+                    i have played a good amount are team fortress 2 and splatoon 1/2. however, of all the fps games i have played, none
+                    can compare to the enjoyment i get from overwatch. you may think i sound like a crazy person; don't all overwatch
+                    players hate the game, yet still play it? sure, there can be frustrating moments, and it's not fun to get a toxic,
+                    throwing teammate. also, it's lame waiting for years for any new content due to blizzard's inabiliy to act like a
+                    modern game develoer. despite all that, i have yet to play a game that can replicate the feelings i get when i win a
+                    really close game of overwatch. i have been playing overwatch ever since launch, but i rarely dabbled into competitive
+                    play until around 2019, and ever since then, i have rarely touched quick play. since 2019, i have climbed from mid plat
+                    on all roles, to reaching masters on support, as well as diamond on both dps and tank. my main heroes are ana, baptiste,
+                    lucio, and zenyatta, but i believe i can play a good amount of the heroes at a fairly decent level at this point. eventually,
+                    i would like to reach grandmaster on support, as well as masters rank on dps and tank, but i still have a long way to go. 
+                </p>
             </div>
             <div className="v-vid">
-                <iframe width="100%" height="100%" src="https://www.youtube.com/embed/1GRBPK9lsTc?start=408" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                <iframe
+                    width="100%" 
+                    height="100%" 
+                    src="https://www.youtube.com/embed/bVFld5G1Lac" 
+                    title="YouTube video player" 
+                    frameborder="0" 
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                    allowfullscreen>
+                </iframe>
             </div>
             <div className="v-vid">
                 <iframe 
@@ -68,7 +91,7 @@ const Achievements = () => {
             </div>
             <div className="v-desc">
                 <h2 className="v-desc-title">
-                    number 2: Punched Seesaws - 33330 (wr)
+                    number 2: Punched Seesaws - 33330 (WR)
                 </h2>
                 <p className="v-desc-desc">
                     of all my records in super monkey ball 2, this is definitely my favorite.
@@ -86,8 +109,35 @@ const Achievements = () => {
             </div>
             <div className="v-desc">
                 <h2 className="v-desc-title">
-                    number 1: waluigi stadium (glitch) - 1:45.860
+                    number 1: GCN Waluigi Stadium (Glitch) - 1:45.860
                 </h2>
+                <p className="v-desc-desc">
+                    of all the crazy things i have done in video games, nothing has ever come close to the satisfaction and happniess i felt
+                    when i got this time on Waluigi Stadium. as you may know, mario kart wii is my all time favorite game. this is because it
+                    was the first game i really got into competitively. not many people know this, but even to this day, mario kart wii has an
+                    online competitive scene, and back in the day, i used to join 'clans,' and we would do clan wars. this was back in 2011 and
+                    2012. however, during this time period, i did not really do too many time trials, as i was more into the online scene. since
+                    then, however, i picked up monkey ball as my speed game, and came to enjoy doing ils. well, in mario kart, the equivalent to ils is
+                    time trials, and ever since i started playing monkey ball, i have been invested in following the time trial scene for mario kart
+                    wii. getting a world record in mario kart wii is one of those things that i always thought would be cool, but i never really believed
+                    i could ever do it, as my mario kart wii skills are nothing compared to some of the time trialers. in july of 2020, i began to
+                    notice developments for a potential new glitch on waluigi stadium, which caught my attention. finally, after some time, mkwii TASer
+                    Ejay B found a <a
+                        href="https://youtu.be/Ba3doATUncM"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="v-link">
+                        human viable strategy
+                    </a> for the ultra shortcut, which would save multiple seconds on the human record if done properly. for one reason or another, i
+                    became fasinated with this glitch, and decided to make it a goal of mine to pull it off; getting wr would have been a nice bouns.
+                    i even kept track of good attempts: <strong>588 total bypasses</strong> (using the finish line to clip through the track), <strong>310 
+                    "good" bypasses</strong> (bypasses that were close to the spot where the glitch is performed), <strong>18 bypasses where I hit the spot
+                    and mistimed the hop,</strong> and <strong>2 times where I actually got the glitch but with no lap count.</strong> after griding attempts
+                    of this glitch for two weeks straight, many hours a day, at around 2:30 in the morning, i pulled it off the 1:45.860 while in a discord call
+                    with one of my friends. it was really late, so i ended up having the most intense yet quiet pop off of my life after crossing the finish line
+                    on lap 3. this was most likely the last time in my life i will ever have the time to grind in a video game super hard, so for that reason, 
+                    i don't think any future achievement in a game will ever top this one.
+                </p>
             </div>
             <div className="v-vid">
                 <iframe
