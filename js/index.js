85c42f489b62d86dee4a7ac860c0a6246e111b8d
tony-montemuro <tonyamontemuro@gmail.com>
2024-01-06 17:43:19 -0600

    set up about page

---
+++ js/index.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("navbar").innerHTML = `
+        <nav>
+            <ul>
+                <li><a href="/">Home</a></li>
+                <li><a href="/about.html">About</a></li>
+                <li><a href="/projects.html">Projects</a></li>
+                <li><a href="/achievements.html">Gaming Achievements</a></li>
+            </ul>
+        </nav>
+    `;
+});
