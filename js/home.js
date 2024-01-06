85e6844a26ea726c4f76499055f98a07a032402d
tony-montemuro <tonyamontemuro@gmail.com>
2024-01-05 19:34:45 -0600

    added navbar to app

---
+++ js/home.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("navbar").innerHTML = `
+        <nav>
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="about">About</a></li>
+                <li><a href="projects">Projects</a></li>
+                <li><a href="achievements">Gaming Achievements</a></li>
+            </ul>
+        </nav>
+    `;
+});
