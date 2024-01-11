83efbcf492fc0a47d85b1a06539e034151493190
tony-montemuro <tonyamontemuro@gmail.com>
2024-01-10 18:37:42 -0600

    added js to handle opening and closing the email modal

--- js/home.js
+++ js/home.js
@@ -1,4 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
+    /* ===== FUNCTIONS ===== */
+
+    // function that will toggle the popup on and off
+    const togglePopup = () => {
+        const modal = document.getElementById("modal-bg");
+        const classes = modal.classList;
+        if (classes.contains("modal-hide")) {
+            classes.remove("modal-hide");
+            classes.add("modal-shown");
+        } else {
+            classes.remove("modal-shown");
+            classes.add("modal-hide");
+        }
+    };
+
+    /* ===== EVENT LISTENERS ===== */
+
     // add event listener for venmo button
     const venmoBtn = document.getElementById("venmo");
     venmoBtn.addEventListener("click", () => {
@@ -10,4 +27,12 @@ document.addEventListener("DOMContentLoaded", () => {
     bitcoinBtn.addEventListener("click", () => {
         alert("Bitcoin wallet: 1P4KZX5BNqHwhzJuaTxrDQ6G9UQ4QVoDmZ");
     });
+
+    // add event listener for email button
+    const emailBtn = document.getElementById("email");
+    emailBtn.addEventListener("click", togglePopup);
+
+    // add event listener for close btn
+    const closeBtn = document.getElementById("close-btn");
+    closeBtn.addEventListener("click", togglePopup);
 });
