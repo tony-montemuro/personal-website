cc8bbb8faa4aaf1f804025b1842e420f93431a5d
tony-montemuro <tonyamontemuro@gmail.com>
2024-01-19 14:58:15 -0600

    made birthday dynamic

--- js/about.js
+++ js/about.js
@@ -1,20 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const carousels = document.querySelectorAll(".about-carousel");
+const birthday = new Date("2000-08-22"), today = new Date();
+let diff = (today.getTime()-birthday.getTime()) / 1000;
+diff /= (60*60*24);
+diff = Math.round(diff/365.25);
+document.getElementById("birthday").innerHTML = `${diff}-year old male`;
 
-    carousels.forEach(carousel => {
-        const slides = carousel.querySelector(".slides");
-        const slide = carousel.querySelector(".slide");
-        const prevBtn = carousel.querySelector(".prev");
-        const nextBtn = carousel.querySelector(".next");
+const carousels = document.querySelectorAll(".about-carousel");
+carousels.forEach(carousel => {
+    const slides = carousel.querySelector(".slides");
+    const slide = carousel.querySelector(".slide");
+    const prevBtn = carousel.querySelector(".prev");
+    const nextBtn = carousel.querySelector(".next");
 
-        prevBtn.addEventListener("click", e => {
-            const width = slide.clientWidth;
-            slides.scrollLeft -= width;
-        });
+    prevBtn.addEventListener("click", () => {
+        const width = slide.clientWidth;
+        slides.scrollLeft -= width;
+    });
 
-        nextBtn.addEventListener("click", e => {
-            const width = slide.clientWidth;
-            slides.scrollLeft += width;
-        });
+    nextBtn.addEventListener("click", () => {
+        const width = slide.clientWidth;
+        slides.scrollLeft += width;
     });
 });
